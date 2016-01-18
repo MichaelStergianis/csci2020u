76c21b78ab95685fa1e767ffca3d44a70143d562
Sam McCabe <sam.thomas.mccabe@gmail.com>
2016-01-18 13:36:48 -0500

    Added a simple loop that prints 0-9

--- labs/lab01/java_source_file.java
+++ labs/lab01/java_source_file.java
@@ -2,5 +2,8 @@ public class java_source_file{
 	public static void main(String args[]){
 		System.out.printf("Hello World\n");
 		System.out.printf("Hello World From Sam!\n");
+		for (int x = 0; x < 10; x++){
+			System.out.print(x + ", ");
+		}
 	}
 }
