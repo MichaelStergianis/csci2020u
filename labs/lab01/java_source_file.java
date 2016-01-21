a1863cc6b5e1c2b002c2927b4386f5231723d0a3
MichaelStergianis <michaelstergianis@gmail.com>
2016-01-21 10:26:18 -0500

    removed loop from java_source_file.java

--- labs/lab01/java_source_file.java
+++ labs/lab01/java_source_file.java
@@ -2,8 +2,5 @@ public class java_source_file{
 	public static void main(String args[]){
 		System.out.printf("Hello World\n");
 		System.out.printf("Hello World From Sam!\n");
-		for (int x = 0; x < 10; x++){
-			System.out.print(x + ", ");
-		}
 	}
 }
