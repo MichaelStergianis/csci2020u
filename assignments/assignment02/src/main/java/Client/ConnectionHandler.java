79bdaeb13f4ab95465149387e2b78dbd33764823
Michael Stergianis <michaelstergianis@gmail.com>
2016-03-25 13:19:03 -0400

    Added file template to describe how our server communicates with client

---
+++ assignments/assignment02/src/main/java/Client/ConnectionHandler.java
@@ -0,0 +1,18 @@
+package Client;
+
+import java.net.Socket;
+
+/**
+ * Created by michael on 25/03/16.
+ */
+public class ConnectionHandler {
+    private Socket serverSocket;
+
+    public void recieveDirs(){
+        // This function will connect to our server
+        // when the directories start coming in
+        // they will come in as a comma separated list
+        // print(fileName + ",");
+        // so string.split(","); will be able to be called
+    }
+}
