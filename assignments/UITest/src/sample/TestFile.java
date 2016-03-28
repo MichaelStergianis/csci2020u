5b4e4a3ef161f1abbd4e24424bed9a604f98c481
irfaan91 <irfaan_ff@hotmail.com>
2016-03-28 08:22:23 -0400

    UI Test commit

---
+++ assignments/UITest/src/sample/TestFile.java
@@ -0,0 +1,15 @@
+package sample;
+
+/**
+ * Created by Irfaan on 28/03/2016.
+ */
+public class TestFile {
+    private String fileName;
+
+    public TestFile(String fName) {
+        this.fileName = fName;
+    }
+
+    public String getFileName() {return fileName;}
+    public void setFileName(String fName) {fileName = fName;}
+}
